c7f99cd339edcc6ccb62bfa19123697ef20565f7
Arpan Kumar <arpan.kumar@microchip.com>
2021-08-09 20:51:03 +0530

    [RSTC] Regenerated demo for rstc_rstc_reset_cause_sam_e54_xpro
    MH3-58586
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/rstc/rstc_reset_cause/firmware/src/config/sam_e54_xpro/peripheral/wdt/plib_wdt.c
+++ apps/rstc/rstc_reset_cause/firmware/src/config/sam_e54_xpro/peripheral/wdt/plib_wdt.c
@@ -91,6 +91,12 @@ void WDT_Disable( void )
     }
 }
 
+void WDT_TimeoutPeriodSet(uint8_t TimeoutPeriod)
+{
+    /* Set WDT timeout period */
+    WDT_REGS->WDT_CONFIG = (WDT_REGS->WDT_CONFIG & ~WDT_CONFIG_PER_Msk) | (TimeoutPeriod & WDT_CONFIG_PER_Msk);
+}
+
 /* If application intends to stay in active mode after clearing WDT, then use WDT_Clear API to clear the WDT. This avoids CPU from waiting or stalling for Synchronization.
  * If application intends to enter low power mode after clearing WDT, then use the WDT_ClearWithSync API to clear the WDT.
  */
