f83985f4eaedfe1d05eb283d70ad19265301bcb7
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-06 15:50:29 +0530

    [EIC] Regenerated demo for eic_eic_led_on_off_sam_e54_xpro
    MH3-67727
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/eic/eic_led_on_off/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/eic/eic_led_on_off/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -78,17 +77,19 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_OUT = 0x80080000;
    PORT_REGS->GROUP[1].PORT_PINCFG[31] = 0x7;
 
+   PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x0;
 
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
    PORT_REGS->GROUP[2].PORT_PINCFG[18] = 0x0;
 
+   PORT_REGS->GROUP[2].PORT_PMUX[9] = 0x0;
 
    /************************** GROUP 3 Initialization *************************/
 
 
-}
 
+}
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
