1797f6340f025008d1edf96be46158b4f7379459
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-06 15:30:55 +0530

    [ADC] Regenerated demo for adc_adc_dma_sequencing_sam_e54_xpro
    MH3-67587
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/adc/adc_dma_sequencing/firmware/src/config/sam_e54_xpro/peripheral/adc/plib_adc0.c
+++ apps/adc/adc_dma_sequencing/firmware/src/config/sam_e54_xpro/peripheral/adc/plib_adc0.c
@@ -61,16 +61,16 @@
 // Section: Global Data
 // *****************************************************************************
 // *****************************************************************************
-ADC_CALLBACK_OBJ ADC0_CallbackObject;
+static ADC_CALLBACK_OBJ ADC0_CallbackObject;
 
-#define ADC0_BIASCOMP_POS  (2)
-#define ADC0_BIASCOMP_Msk   (0x7 << ADC0_BIASCOMP_POS)
+#define ADC0_BIASCOMP_POS     (2U)
+#define ADC0_BIASCOMP_Msk     (0x7U << ADC0_BIASCOMP_POS)
 
-#define ADC0_BIASREFBUF_POS  (5)
-#define ADC0_BIASREFBUF_Msk   (0x7 << ADC0_BIASREFBUF_POS)
+#define ADC0_BIASREFBUF_POS   (5U)
+#define ADC0_BIASREFBUF_Msk   (0x7U << ADC0_BIASREFBUF_POS)
 
-#define ADC0_BIASR2R_POS  (8)
-#define ADC0_BIASR2R_Msk   (0x7 << ADC0_BIASR2R_POS)
+#define ADC0_BIASR2R_POS      (8U)
+#define ADC0_BIASR2R_Msk      (0x7UL << ADC0_BIASR2R_POS)
 
 
 // *****************************************************************************
@@ -92,9 +92,9 @@ void ADC0_Initialize( void )
     }
 
     /* Writing calibration values in BIASREFBUF, BIASCOMP and BIASR2R */
-    ADC0_REGS->ADC_CALIB =(uint32_t)(ADC_CALIB_BIASCOMP((((*(uint64_t*)SW0_ADDR) & ADC0_BIASCOMP_Msk) >> ADC0_BIASCOMP_POS))) \
-            | ADC_CALIB_BIASR2R((((*(uint64_t*)SW0_ADDR) & ADC0_BIASR2R_Msk) >> ADC0_BIASR2R_POS))
-            | ADC_CALIB_BIASREFBUF(((*(uint64_t*)SW0_ADDR) & ADC0_BIASREFBUF_Msk)>> ADC0_BIASREFBUF_POS );
+    ADC0_REGS->ADC_CALIB =(uint16_t)((ADC_CALIB_BIASCOMP((((*(uint32_t*)SW0_ADDR) & ADC0_BIASCOMP_Msk) >> ADC0_BIASCOMP_POS))) \
+            | ADC_CALIB_BIASR2R((((*(uint32_t*)SW0_ADDR) & ADC0_BIASR2R_Msk) >> ADC0_BIASR2R_POS))
+            | ADC_CALIB_BIASREFBUF(((*(uint32_t*)SW0_ADDR) & ADC0_BIASREFBUF_Msk)>> ADC0_BIASREFBUF_POS ));
 
     /* prescaler */
     ADC0_REGS->ADC_CTRLA = ADC_CTRLA_PRESCALER_DIV4;
@@ -111,7 +111,7 @@ void ADC0_Initialize( void )
     ADC0_REGS->ADC_INPUTCTRL = (uint16_t) ADC_POSINPUT_AIN0 | (uint16_t) ADC_NEGINPUT_GND ;
 
     /* Resolution & Operation Mode */
-    ADC0_REGS->ADC_CTRLB = ADC_CTRLB_RESSEL_12BIT | ADC_CTRLB_WINMODE(0) ;
+    ADC0_REGS->ADC_CTRLB = ADC_CTRLB_RESSEL_12BIT | ADC_CTRLB_WINMODE(0U) ;
 
 
     /* Clear all interrupt flags */
@@ -121,7 +121,7 @@ void ADC0_Initialize( void )
     /* Events configuration  */
     ADC0_REGS->ADC_EVCTRL = ADC_EVCTRL_STARTEI_Msk;
 
-    while(ADC0_REGS->ADC_SYNCBUSY)
+    while(ADC0_REGS->ADC_SYNCBUSY != 0U)
     {
         /* Wait for Synchronization */
     }
@@ -131,7 +131,7 @@ void ADC0_Initialize( void )
 void ADC0_Enable( void )
 {
     ADC0_REGS->ADC_CTRLA |= ADC_CTRLA_ENABLE_Msk;
-    while(ADC0_REGS->ADC_SYNCBUSY)
+    while(ADC0_REGS->ADC_SYNCBUSY != 0U)
     {
         /* Wait for Synchronization */
     }
@@ -140,8 +140,8 @@ void ADC0_Enable( void )
 /* Disable ADC module */
 void ADC0_Disable( void )
 {
-    ADC0_REGS->ADC_CTRLA &= ~ADC_CTRLA_ENABLE_Msk;
-    while(ADC0_REGS->ADC_SYNCBUSY)
+    ADC0_REGS->ADC_CTRLA &=(uint16_t) ~ADC_CTRLA_ENABLE_Msk;
+    while(ADC0_REGS->ADC_SYNCBUSY != 0U)
     {
         /* Wait for Synchronization */
     }
@@ -151,9 +151,9 @@ void ADC0_Disable( void )
 void ADC0_ChannelSelect( ADC_POSINPUT positiveInput, ADC_NEGINPUT negativeInput )
 {
     /* Configure positive and negative input pins */
-    uint32_t channel;
+    uint16_t channel;
     channel = ADC0_REGS->ADC_INPUTCTRL;
-    channel &= ~(ADC_INPUTCTRL_MUXPOS_Msk | ADC_INPUTCTRL_MUXNEG_Msk);
+    channel &= (uint16_t)~(ADC_INPUTCTRL_MUXPOS_Msk | ADC_INPUTCTRL_MUXNEG_Msk);
     channel |= (uint16_t) positiveInput | (uint16_t) negativeInput;
     ADC0_REGS->ADC_INPUTCTRL = channel;
 
@@ -180,7 +180,7 @@ void ADC0_ComparisonWindowSet(uint16_t low_threshold, uint16_t high_threshold)
 {
     ADC0_REGS->ADC_WINLT = low_threshold;
     ADC0_REGS->ADC_WINUT = high_threshold;
-    while((ADC0_REGS->ADC_SYNCBUSY))
+    while(ADC0_REGS->ADC_SYNCBUSY != 0U)
     {
         /* Wait for Synchronization */
     }
@@ -188,9 +188,9 @@ void ADC0_ComparisonWindowSet(uint16_t low_threshold, uint16_t high_threshold)
 
 void ADC0_WindowModeSet(ADC_WINMODE mode)
 {
-    ADC0_REGS->ADC_CTRLB &= ~ADC_CTRLB_WINMODE_Msk;
-    ADC0_REGS->ADC_CTRLB |= mode << ADC_CTRLB_WINMODE_Pos;
-    while((ADC0_REGS->ADC_SYNCBUSY))
+    ADC0_REGS->ADC_CTRLB &= (uint16_t)~ADC_CTRLB_WINMODE_Msk;
+    ADC0_REGS->ADC_CTRLB |= (uint16_t)mode << ADC_CTRLB_WINMODE_Pos;
+    while(ADC0_REGS->ADC_SYNCBUSY != 0U)
     {
         /* Wait for Synchronization */
     }
--- apps/adc/adc_dma_sequencing/firmware/src/config/sam_e54_xpro/peripheral/nvic/plib_nvic.c
+++ apps/adc/adc_dma_sequencing/firmware/src/config/sam_e54_xpro/peripheral/nvic/plib_nvic.c
@@ -66,7 +66,13 @@ void NVIC_Initialize( void )
     NVIC_SetPriority(ADC0_RESRDY_IRQn, 7);
     NVIC_EnableIRQ(ADC0_RESRDY_IRQn);
 
+    /* Enable Usage fault */
+    SCB->SHCSR |= (SCB_SHCSR_USGFAULTENA_Msk);
+    /* Trap divide by zero */
+    SCB->CCR   |= SCB_CCR_DIV_0_TRP_Msk;
 
+    /* Enable Bus fault */
+    SCB->SHCSR |= (SCB_SHCSR_BUSFAULTENA_Msk);
 
 }
 
@@ -78,9 +84,7 @@ void NVIC_INT_Enable( void )
 
 bool NVIC_INT_Disable( void )
 {
-    bool processorStatus;
-
-    processorStatus = (bool) (__get_PRIMASK() == 0);
+    bool processorStatus = (__get_PRIMASK() == 0U);
 
     __disable_irq();
     __DMB();
