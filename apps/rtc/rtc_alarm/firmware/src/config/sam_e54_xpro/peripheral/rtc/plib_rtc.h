9f12365697021a85a57e66facd178773716a01a5
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-06 16:33:34 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_sam_e54_xpro
    MH3-67738
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/rtc/rtc_alarm/firmware/src/config/sam_e54_xpro/peripheral/rtc/plib_rtc.h
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_e54_xpro/peripheral/rtc/plib_rtc.h
@@ -106,14 +106,12 @@ typedef enum
     BACKUP_REGISTER_6 = 6U,
     BACKUP_REGISTER_7 = 7U
 } BACKUP_REGISTER;
-typedef enum
-{
-    TAMPER_CHANNEL_0 = 0U,
-    TAMPER_CHANNEL_1 = 1U,
-    TAMPER_CHANNEL_2 = 2U,
-    TAMPER_CHANNEL_3 = 3U,
-    TAMPER_CHANNEL_4 = 4U,
-} TAMPER_CHANNEL;
+ #define   TAMPER_CHANNEL_0  (0U)
+ #define   TAMPER_CHANNEL_1  (1U)
+ #define   TAMPER_CHANNEL_2  (2U)
+ #define   TAMPER_CHANNEL_3  (3U)
+ #define   TAMPER_CHANNEL_4  (4U)
+typedef uint32_t TAMPER_CHANNEL;
 typedef void (*RTC_CALLBACK)( RTC_CLOCK_INT_MASK intCause, uintptr_t context );
 
 typedef struct
@@ -125,6 +123,8 @@ typedef struct
 } RTC_OBJECT;
 
 void RTC_Initialize(void);
+void RTC_RTCCClockSyncEnable ( void );
+void RTC_RTCCClockSyncDisable ( void );
 bool RTC_RTCCTimeSet (struct tm * initialTime );
 void RTC_RTCCTimeGet ( struct tm * currentTime );
 bool RTC_RTCCAlarm0Set ( struct tm * alarmTime, RTC_ALARM_MASK mask );
