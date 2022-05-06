e8efbedc1f1333a8c5f1cf1c674665b44aca6825
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-06 16:44:40 +0530

    [SERCOM] Regenerated demo for sercom_spi_master_spi_eeprom_write_read_sam_e54_xpro
    MH3-67744
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/sercom/spi/master/spi_eeprom_write_read/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/master/spi_eeprom_write_read/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -80,6 +79,8 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_PINCFG[1] = 0x0;
    PORT_REGS->GROUP[1].PORT_PINCFG[14] = 0x0;
 
+   PORT_REGS->GROUP[1].PORT_PMUX[0] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PMUX[7] = 0x0;
 
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40040;
@@ -92,12 +93,13 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[2].PORT_PMUX[2] = 0x22;
    PORT_REGS->GROUP[2].PORT_PMUX[3] = 0x20;
+   PORT_REGS->GROUP[2].PORT_PMUX[9] = 0x0;
 
    /************************** GROUP 3 Initialization *************************/
 
 
-}
 
+}
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
