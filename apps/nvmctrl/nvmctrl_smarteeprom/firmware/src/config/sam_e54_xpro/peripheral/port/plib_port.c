239be245f8c0b88da018720f1525b0462c1aa0d0
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-04-19 14:54:54 +0530

    [NVMCTRL] Regenerated demo for nvmctrl_nvmctrl_smarteeprom_sam_e54_xpro
    MH3-79198

--- apps/nvmctrl/nvmctrl_smarteeprom/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/nvmctrl/nvmctrl_smarteeprom/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -74,17 +74,17 @@ void PORT_Initialize(void)
 
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x1;
-   PORT_REGS->GROUP[1].PORT_PINCFG[25] = 0x1;
+   PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x1U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[25] = 0x1U;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[12] = 0x33;
+   PORT_REGS->GROUP[1].PORT_PMUX[12] = 0x33U;
 
    /************************** GROUP 2 Initialization *************************/
-   PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
-   PORT_REGS->GROUP[2].PORT_OUT = 0x40000;
-   PORT_REGS->GROUP[2].PORT_PINCFG[18] = 0x0;
+   PORT_REGS->GROUP[2].PORT_DIR = 0x40000U;
+   PORT_REGS->GROUP[2].PORT_OUT = 0x40000U;
+   PORT_REGS->GROUP[2].PORT_PINCFG[18] = 0x0U;
 
-   PORT_REGS->GROUP[2].PORT_PMUX[9] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PMUX[9] = 0x0U;
 
    /************************** GROUP 3 Initialization *************************/
 
