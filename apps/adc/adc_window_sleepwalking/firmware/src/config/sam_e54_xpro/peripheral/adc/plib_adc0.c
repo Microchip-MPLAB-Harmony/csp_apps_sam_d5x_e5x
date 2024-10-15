f2fe64074dc056abc448bc93c49bccb410c7ea51
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-15 15:43:22 +0530

    [ADC] Regenerated demo for adc__adc_window_sleepwalking__firmware__sam_e54_xpro.X
    MH3-101766

--- apps/adc/adc_window_sleepwalking/firmware/src/config/sam_e54_xpro/peripheral/adc/plib_adc0.c
+++ apps/adc/adc_window_sleepwalking/firmware/src/config/sam_e54_xpro/peripheral/adc/plib_adc0.c
@@ -100,10 +100,10 @@ void ADC0_Initialize( void )
     ADC0_REGS->ADC_CTRLA = ADC_CTRLA_PRESCALER_DIV16;
 
     /* Sampling length */
-    ADC0_REGS->ADC_SAMPCTRL = ADC_SAMPCTRL_SAMPLEN(3U);
+    ADC0_REGS->ADC_SAMPCTRL = (uint8_t)ADC_SAMPCTRL_SAMPLEN(3UL);
 
     /* reference */
-    ADC0_REGS->ADC_REFCTRL = ADC_REFCTRL_REFSEL_INTVCC1;
+    ADC0_REGS->ADC_REFCTRL = ADC_REFCTRL_REFSEL_INTVCC1 | ADC_REFCTRL_REFCOMP_Msk;
 
 
     /* positive and negative input pins */
@@ -135,7 +135,7 @@ void ADC0_Initialize( void )
 void ADC0_Enable( void )
 {
     ADC0_REGS->ADC_CTRLA |= ADC_CTRLA_ENABLE_Msk;
-    while(ADC0_REGS->ADC_SYNCBUSY != 0U)
+    while((ADC0_REGS->ADC_SYNCBUSY & ADC_SYNCBUSY_ENABLE_Msk) == ADC_SYNCBUSY_ENABLE_Msk)
     {
         /* Wait for Synchronization */
     }
@@ -145,7 +145,7 @@ void ADC0_Enable( void )
 void ADC0_Disable( void )
 {
     ADC0_REGS->ADC_CTRLA &=(uint16_t) ~ADC_CTRLA_ENABLE_Msk;
-    while(ADC0_REGS->ADC_SYNCBUSY != 0U)
+    while((ADC0_REGS->ADC_SYNCBUSY & ADC_SYNCBUSY_ENABLE_Msk) == ADC_SYNCBUSY_ENABLE_Msk)
     {
         /* Wait for Synchronization */
     }
@@ -184,17 +184,21 @@ void ADC0_ComparisonWindowSet(uint16_t low_threshold, uint16_t high_threshold)
 {
     ADC0_REGS->ADC_WINLT = low_threshold;
     ADC0_REGS->ADC_WINUT = high_threshold;
-    while(ADC0_REGS->ADC_SYNCBUSY != 0U)
+    while((ADC0_REGS->ADC_SYNCBUSY & ADC_SYNCBUSY_WINLT_Msk) == ADC_SYNCBUSY_WINLT_Msk)
     {
         /* Wait for Synchronization */
     }
+    while((ADC0_REGS->ADC_SYNCBUSY & ADC_SYNCBUSY_WINUT_Msk) == ADC_SYNCBUSY_WINUT_Msk)
+    {
+        /* Wait for Synchronization */
+    } 
 }
 
 void ADC0_WindowModeSet(ADC_WINMODE mode)
 {
     ADC0_REGS->ADC_CTRLB &= (uint16_t)~ADC_CTRLB_WINMODE_Msk;
     ADC0_REGS->ADC_CTRLB |= (uint16_t)mode << ADC_CTRLB_WINMODE_Pos;
-    while(ADC0_REGS->ADC_SYNCBUSY != 0U)
+    while((ADC0_REGS->ADC_SYNCBUSY & ADC_SYNCBUSY_CTRLB_Msk) == ADC_SYNCBUSY_CTRLB_Msk)
     {
         /* Wait for Synchronization */
     }
@@ -240,8 +244,11 @@ bool ADC0_ConversionStatusGet( void )
 {
     bool status;
     status =  (((ADC0_REGS->ADC_INTFLAG & ADC_INTFLAG_RESRDY_Msk) >> ADC_INTFLAG_RESRDY_Pos) !=0U);
-    /* Clear interrupt flag */
-    ADC0_REGS->ADC_INTFLAG = ADC_INTFLAG_RESRDY_Msk;
+    if (status == true)
+    {
+        /* Clear interrupt flag */
+        ADC0_REGS->ADC_INTFLAG = ADC_INTFLAG_RESRDY_Msk;
+    }
     return status;
 }
 void __attribute__((used)) ADC0_OTHER_InterruptHandler( void )
