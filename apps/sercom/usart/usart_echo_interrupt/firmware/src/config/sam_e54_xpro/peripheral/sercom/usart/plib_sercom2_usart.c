37959f7f45ac07c7a58c8df4ab3474e8b240c72e
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-15 15:43:38 +0530

    [SERCOM] Regenerated demo for sercom__usart__usart_echo_interrupt__firmware__sam_e54_xpro.X
    MH3-101836

--- apps/sercom/usart/usart_echo_interrupt/firmware/src/config/sam_e54_xpro/peripheral/sercom/usart/plib_sercom2_usart.c
+++ apps/sercom/usart/usart_echo_interrupt/firmware/src/config/sam_e54_xpro/peripheral/sercom/usart/plib_sercom2_usart.c
@@ -47,21 +47,18 @@
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-
 #include "interrupts.h"
 #include "plib_sercom2_usart.h"
-
 // *****************************************************************************
 // *****************************************************************************
 // Section: Global Data
 // *****************************************************************************
 // *****************************************************************************
-
-
 /* SERCOM2 USART baud value for 115200 Hz baud rate */
 #define SERCOM2_USART_INT_BAUD_VALUE            (63522UL)
 
 volatile static SERCOM_USART_OBJECT sercom2USARTObj;
+
 
 // *****************************************************************************
 // *****************************************************************************
@@ -102,6 +99,7 @@ void SERCOM2_USART_Initialize( void )
      * Configures Sampling rate
      * Configures IBON
      */
+
     SERCOM2_REGS->USART_INT.SERCOM_CTRLA = SERCOM_USART_INT_CTRLA_MODE_USART_INT_CLK | SERCOM_USART_INT_CTRLA_RXPO(0x1UL) | SERCOM_USART_INT_CTRLA_TXPO(0x0UL) | SERCOM_USART_INT_CTRLA_DORD_Msk | SERCOM_USART_INT_CTRLA_IBON_Msk | SERCOM_USART_INT_CTRLA_FORM(0x0UL) | SERCOM_USART_INT_CTRLA_SAMPR(0UL) ;
 
     /* Configure Baud Rate */
@@ -146,6 +144,10 @@ void SERCOM2_USART_Initialize( void )
     sercom2USARTObj.errorStatus = USART_ERROR_NONE;
 }
 
+
+
+
+
 uint32_t SERCOM2_USART_FrequencyGet( void )
 {
     return 60000000UL;
@@ -156,6 +158,7 @@ bool SERCOM2_USART_SerialSetup( USART_SERIAL_SETUP * serialSetup, uint32_t clkFr
     bool setupStatus       = false;
     uint32_t baudValue     = 0U;
     uint32_t sampleRate    = 0U;
+    uint32_t sampleCount   = 0U;
 
     bool transferProgress = sercom2USARTObj.txBusyStatus;
     transferProgress = sercom2USARTObj.rxBusyStatus || transferProgress;
@@ -174,24 +177,24 @@ bool SERCOM2_USART_SerialSetup( USART_SERIAL_SETUP * serialSetup, uint32_t clkFr
 
         if(clkFrequency >= (16U * serialSetup->baudRate))
         {
-            baudValue = 65536U - (uint32_t)(((uint64_t)65536U * 16U * serialSetup->baudRate) / clkFrequency);
             sampleRate = 0U;
+            sampleCount = 16U;
         }
         else if(clkFrequency >= (8U * serialSetup->baudRate))
         {
-            baudValue = 65536U - (uint32_t)(((uint64_t)65536U * 8U * serialSetup->baudRate) / clkFrequency);
             sampleRate = 2U;
+            sampleCount = 8U;
         }
         else if(clkFrequency >= (3U * serialSetup->baudRate))
         {
-            baudValue = 65536U - (uint32_t)(((uint64_t)65536U * 3U * serialSetup->baudRate) / clkFrequency);
             sampleRate = 4U;
+            sampleCount = 3U;
         }
         else
         {
             /* Do nothing */
         }
-
+        baudValue = 65536U - (uint32_t)(((uint64_t)65536U * sampleCount * serialSetup->baudRate) / clkFrequency);
         /* Disable the USART before configurations */
         SERCOM2_REGS->USART_INT.SERCOM_CTRLA &= ~SERCOM_USART_INT_CTRLA_ENABLE_Msk;
 
@@ -606,3 +609,7 @@ void __attribute__((used)) SERCOM2_USART_InterruptHandler( void )
         }
     }
 }
+
+
+
+
